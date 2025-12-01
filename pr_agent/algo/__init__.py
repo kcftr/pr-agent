46f7b9fa0a2ea32172eaf3faff41f579c4a10cc0
KC <130642707+kcftr@users.noreply.github.com>
2025-12-02 02:32:06 +0800

    Add gemini/gemini-3-pro-preview to size mapping

--- pr_agent/algo/__init__.py
+++ pr_agent/algo/__init__.py
@@ -90,6 +90,7 @@ MAX_TOKENS = {
     'gemini/gemini-2.5-pro-preview-05-06': 1048576,
     'gemini/gemini-2.5-pro-preview-06-05': 1048576,
     'gemini/gemini-2.5-pro': 1048576,
+    'gemini/gemini-3-pro-preview': 1048576,
     'codechat-bison': 6144,
     'codechat-bison-32k': 32000,
     'anthropic.claude-instant-v1': 100000,
